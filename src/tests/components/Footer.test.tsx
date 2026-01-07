83a0741bde7f2ee871117b2e8481095dcc4c561f
tech-blackboard <techblackboard@gmail.com>
2026-01-07 15:21:44 +0530

    huskey commit

--- src/tests/components/Footer.test.tsx
+++ src/tests/components/Footer.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import Footer from '../components/Footer.tsx'
+import Footer from '../../src/components/Footer.tsx'
 
 describe('Footer component', () => {
   test('renders footer text with current year', () => {
