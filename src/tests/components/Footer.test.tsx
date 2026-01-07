3744fc67017b7c68e3046185d36e32309ebf5953
tech-blackboard <techblackboard@gmail.com>
2026-01-07 16:13:13 +0530

    test

--- src/tests/components/Footer.test.tsx
+++ src/tests/components/Footer.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import Footer from '../../components/Footersss.tsx'
+import Footer from '../../components/Footerssss.tsx'
 
 describe('Footer component', () => {
   test('renders footer text with current year', () => {
